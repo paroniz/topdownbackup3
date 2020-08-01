0d51b11b06b7c8fe291f99199cf3f63b40bd5feb
Imre Kinnunen <imrekinnunen@hotmail.com>
2020-08-01 21:55:45 +0300

    moi taas

---
+++ Assets/koodit/salaovitrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salaovitrigger : MonoBehaviour {
+    
+    private GameObject ovi;
+    private GameObject ovi2;
+    void Start() {
+        ovi = GameObject.Find("ykssuuntanenovi");
+        ovi2 = GameObject.Find("ykssuuntainenovi2");
+    }
+
+    void Update() {}
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "hero")
+        {
+            Debug.Log("avaa");
+            ovi.GetComponent<Collider2D>().enabled = false;
+            ovi2.GetComponent<Collider2D>().enabled = false;
+        }
+    }  
+    
+}
