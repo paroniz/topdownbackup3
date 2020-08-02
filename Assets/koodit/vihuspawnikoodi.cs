dbcd7101ff688546eb5cdaa4f59fd02e43200f32
Imre Kinnunen <imrekinnunen@hotmail.com>
2020-08-02 13:36:14 +0300

    moi taas

---
+++ Assets/koodit/vihuspawnikoodi.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vihuspawnikoodi : MonoBehaviour {
+    
+    void Start() {}
+
+    void Update() {}
+}
